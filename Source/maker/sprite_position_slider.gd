e41df12c12e863f2e2a8b1d9d5867bf9b954f85d
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-07-29 14:59:22 -0700

    fix: Fix issue with sprite position slider not updating properly.

--- Source/maker/sprite_position_slider.gd
+++ Source/maker/sprite_position_slider.gd
@@ -6,6 +6,10 @@ extends HSlider
 func _ready():
 	value = controller.target.position.y
 	value_changed.connect(_on_value_changed)
+	controller.target_changed.connect(_update_target)
 
 func _on_value_changed(value):
 	controller.target.position.y = value
+
+func _update_target(target : MonsterComponent):
+	value = target.position.y

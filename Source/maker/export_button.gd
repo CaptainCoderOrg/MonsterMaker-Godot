d026376dd2c40bb442d1867733fd7c1bc3b61c99
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-07-30 06:10:55 -0700

    feat: Added export feature.

--- Source/maker/export_button.gd
+++ Source/maker/export_button.gd
@@ -3,9 +3,9 @@ extends Button
 @onready var sub_viewport = %SubViewport
 @onready var monster = %Monster
 @onready var monster_container = %MonsterContainer
+@onready var file_dialog :FileDialog = %FileDialog
 
-
-func _on_pressed():
+func export_file(path : String):
 	for child in monster_container.get_children():
 		child.queue_free()
 	var duplicate : Node2D = monster.duplicate() as Node2D
@@ -13,5 +13,10 @@ func _on_pressed():
 	monster_container.add_child(duplicate)
 	var texture : ViewportTexture  = sub_viewport.get_texture()
 	await get_tree().create_timer(1).timeout
-	texture.get_image().save_png("user://export.png")
+	texture.get_image().save_png(path)
 	
+func _on_pressed():
+	file_dialog.show()
+
+func _on_file_dialog_confirmed():
+	export_file(file_dialog.current_path)

97dbc1d93ea9c97b1007c6af7878faa0d0d36b4a
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-07-30 07:12:07 -0700

    feat/chore: Add JavaScript Export / Update project export settings.

--- Source/maker/export_button.gd
+++ Source/maker/export_button.gd
@@ -5,7 +5,7 @@ extends Button
 @onready var monster_container = %MonsterContainer
 @onready var file_dialog :FileDialog = %FileDialog
 
-func export_file(path : String):
+func generate_texture() -> ViewportTexture:
 	for child in monster_container.get_children():
 		child.queue_free()
 	var duplicate : Node2D = monster.duplicate() as Node2D
@@ -13,10 +13,22 @@ func export_file(path : String):
 	monster_container.add_child(duplicate)
 	var texture : ViewportTexture  = sub_viewport.get_texture()
 	await get_tree().create_timer(1).timeout
+	return texture
+
+func download_image():
+	var texture = await generate_texture()
+	var buf = texture.get_image().save_png_to_buffer()
+	JavaScriptBridge.download_buffer(buf, "export.png", "image/png")
+
+func export_file(path : String):
+	var texture = await generate_texture()
 	texture.get_image().save_png(path)
 	
 func _on_pressed():
-	file_dialog.show()
+	if OS.has_feature("web"):
+		download_image()
+	else:
+		file_dialog.show()
 
 func _on_file_dialog_confirmed():
 	export_file(file_dialog.current_path)
